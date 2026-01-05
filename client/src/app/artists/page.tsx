bb7d06b95784c2e011d5590348f8b072658f87bd
Cameron Alexander <emptyflash@gmail.com>
2026-01-04 21:33:59 -0500

    Finish mediums query

--- client/src/app/artists/page.tsx
+++ client/src/app/artists/page.tsx
@@ -1,33 +1,70 @@
+"use client";
+
+import React, {useState, useEffect} from "react";
 import Link from "next/link";
 import { type SanityDocument } from "next-sanity";
 
 import { client } from "@/sanity/client";
 
-const ARTIST_QUERY = `*[
+const ARTIST_QUERY = (filters: string[]) => `*[
   _type == "artist"
+  ${filters.length > 0 ? ' && ' + filters.join("&&") : ''}
   && defined(slug.current)
-][0...12]{_id, name, slug}`;
+][0...12]`;
 
 const options = { next: { revalidate: 30 } };
 
-export default async function ArtistsIndexPage() {
-  const artists = await client.fetch<SanityDocument[]>(
-    ARTIST_QUERY,
-    {},
-    options,
-  );
+const MEDIUM_QUERY = `
+array::unique(*[_type == "artist"].medium[])
+`
+
+export default function ArtistsIndexPage() {
+  const [mediumFilter, setMediumFilter] = useState<string[]>([]);
+  const [mediumOptions, setMediumOptions] = useState<string[] | null>(null);
+  useEffect(() => {
+    client.fetch(
+      MEDIUM_QUERY,
+      {},
+      options,
+    ).then(setMediumOptions);
+  }, []);
+
+  const [artists, setArtists] = useState<SanityDocument[] | null>(null);
+  useEffect(() => {
+    const filters = mediumFilter.length > 0 ? [`count(medium[@ in $mediums]) > 0`] : [];
+    const query = ARTIST_QUERY(filters)
+    client.fetch<SanityDocument[]>(
+      query,
+      { mediums: mediumFilter },
+      options,
+    ).then(setArtists);
+  }, [mediumFilter]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const values = Array.from(event.target.selectedOptions).map((option) => option.value);
+    setMediumFilter(values);
+  };
 
   return (
     <main className="container mx-auto min-h-screen max-w-3xl p-8">
       <h1 className="text-4xl font-bold mb-8">Artists</h1>
+      { mediumOptions && 
+        <select multiple={true} value={mediumFilter} onChange={handleChange} id="mediumSelect">
+          {mediumOptions.map((medium: string) => (
+            <option key={medium} value={medium}>
+              {medium}
+            </option>
+          ))}
+        </select>
+      }
       <ul className="flex flex-col gap-y-4">
-        {artists.map((artist) => (
+        {artists !== null ? artists.map((artist) => (
           <li className="hover:underline" key={artist._id}>
             <Link href={`/artists/${artist.slug.current}`}>
               <h2 className="text-xl font-semibold">{artist.name}</h2>
             </Link>
           </li>
-        ))}
+        )) : <li>Loading...</li>}
       </ul>
     </main>
   );
