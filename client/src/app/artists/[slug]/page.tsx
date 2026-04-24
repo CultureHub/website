b297b8d81fe6d52cc9461320f9665469904fb227
Cameron Alexander <emptyflash@gmail.com>
2026-04-24 17:16:29 -0400

    Make location a list and fix types

--- client/src/app/artists/[slug]/page.tsx
+++ client/src/app/artists/[slug]/page.tsx
@@ -34,7 +34,7 @@ export default async function ArtistPage({
               />
               {artist.locations && <LocationPin locations={artist.locations} />}
             </div>
-            <h1 className="text-4xl font-bold md:my-6">{artist.name}</h1>
+            <h1 className="text-4xl RaRfont-bold md:my-6">{artist.name}</h1>
           </div>
         </div>
         <div className="flex flex-col md:flex-row gap-9 md:gap-14">
