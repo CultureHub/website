5e1197e736511221a7dbe0bdc19738598f7c84aa
Cameron Alexander <emptyflash@gmail.com>
2026-07-10 15:44:30 -0400

    refactor: use CreditSection component on project detail page

--- client/src/app/projects/[slug]/page.tsx
+++ client/src/app/projects/[slug]/page.tsx
@@ -1,18 +1,13 @@
 import React from "react";
-import Link from "next/link";
-import Image from "next/image";
 import { notFound } from "next/navigation";
-import { PortableText, type SanityDocument } from "next-sanity";
-import { getImageDimensions } from "@sanity/asset-utils";
+import { PortableText } from "next-sanity";
 import { getProjectBySlug } from "@/sanity/queries";
-
-import { client } from "@/sanity/client";
-import { urlFor } from "@/sanity/url";
 import ProjectContentRow from "@/components/ProjectContentRow";
 import SanityImage from "@/components/SanityImage";
 import RelatedCarousel from "@/components/RelatedCarousel";
 import Breadcrumbs from "@/components/Breadcrumbs";
 import LocationPin from "@/components/LocationPin";
+import { CreditSection } from "@/components/credits";
 
 export default async function ProjectPage({
   params,
@@ -81,76 +76,7 @@ export default async function ProjectPage({
               <ProjectContentRow key={i} content={content} />
             ))}
 
-          {project.credits && (
-            <div className="flex flex-col gap-[52px] mx-6 md:mx-0">
-              <div className="w-full py-6 border-t border-black">
-                <h3 className="text-3xl font-bold uppercase">Credits</h3>
-              </div>
-
-              {project.credits.description && (
-                <div className="text-2xl font-normal font-brook">
-                  <PortableText value={project.credits.description} />
-                </div>
-              )}
-
-              {project.credits.locations && (
-                <div className="columns-1 md:columns-2 md:gap-x-16">
-                  {project.credits.locations.map((location) => (
-                    <div
-                      key={location._key}
-                      className="flex flex-col gap-6 break-inside-avoid mb-11"
-                    >
-                      <div className="flex flex-row py-2.5 justify-start items-center border-t border-b text-2xl font-normal font-brook uppercase gap-3">
-                        <Image
-                          width="8"
-                          height="14"
-                          src="/pin.svg"
-                          alt="Location pin"
-                        />
-                        <p>{location.name}</p>
-                      </div>
-                      {location.description && (
-                        <div className="text-2xl font-normal font-brook leading-6">
-                          <PortableText value={location.description} />
-                        </div>
-                      )}
-                      {location.organizations &&
-                        location.organizations.map((organization) => (
-                          <div
-                            key={organization._key}
-                            className="flex flex-col gap-4.5"
-                          >
-                            <h4 className="text-2xl font-normal font-brook uppercase">
-                              {organization.name}
-                            </h4>
-                            {organization.description && (
-                              <div className="text-2xl font-normal font-brook leading-6">
-                                <PortableText
-                                  value={organization.description}
-                                />
-                              </div>
-                            )}
-                            <div className="font-milling text-2xl">
-                              {organization.teams &&
-                                organization.teams.map((team) => (
-                                  <div key={team.role}>
-                                    <span className="font-bold">
-                                      {team.role}:{" "}
-                                    </span>
-                                    <span className="font-thin">
-                                      {team.people}
-                                    </span>
-                                  </div>
-                                ))}
-                            </div>
-                          </div>
-                        ))}
-                    </div>
-                  ))}
-                </div>
-              )}
-            </div>
-          )}
+          <CreditSection credits={project.credits} />
 
           {project.related && (
             <div className="flex flex-col gap-[21px] mx-6 md:mx-0">
