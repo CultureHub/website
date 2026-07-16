b7c3f397ce8406670b1a6f52ab328040c5dc2ade
Cameron Alexander <emptyflash@gmail.com>
2026-07-16 12:56:33 -0400

    feat: register event type in schema index

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -2,5 +2,6 @@ import {projectType} from './projectType'
 import {artistType} from './artistType'
 import {programType} from './programType'
 import {artAndTechnologyPageType} from './artAndTechnologyPageType'
+import {eventType} from './eventType'
 
-export const schemaTypes = [projectType, artistType, programType, artAndTechnologyPageType]
+export const schemaTypes = [projectType, artistType, programType, artAndTechnologyPageType, eventType]
