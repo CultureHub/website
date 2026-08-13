236823e2f660c59773742defac0c54d45d9c8183
Cameron Alexander <emptyflash@gmail.com>
2026-08-13 15:27:42 -0400

    Make deprecated program field not required for artists

--- sanity/util/program.ts
+++ sanity/util/program.ts
@@ -4,7 +4,6 @@ export function programField(props: { title: string, name: string }) {
   return {
     type: 'reference',
     to: [{ type: 'program' }],
-    validation: (rule: Rule) => rule.required(),
     ...props,
   }
 }
